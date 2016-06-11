1d2dbe8b35096bd76f8e0c804ae1707caaac182d
potterua <nazarrrka@gmail.com>
2016-06-11 17:54:07 +0300

    [v2.0.0]

--- game.py
+++ game.py
@@ -1,32 +1,10 @@
 from tkinter import *
-from threading import Thread
-from tkinter import messagebox
-import time
 import random
-import sys
 import os
 
-FRAME_TIME = 0.022
+FRAMERATE = 20
+SCORE = -1
 
-current_x = 200
-score = -1
-truba_x = 300
-diraP = 0
-state = ""
-finT1 = finT2 = finR = None
-IS_PAUSE = 0
-
-BEST_SCORE = 0
-
-if os.path.isfile("data.dat"):
-	scoreFile = open('data.dat')
-	BEST_SCORE = int(scoreFile.read())
-	scoreFile.close()
-else:
-	scoreFile = open('data.dat', 'w')
-	scoreFile.write(str(BEST_SCORE))
-	scoreFile.close()
-	
 def center(toplevel):
     toplevel.update_idletasks()
     w = toplevel.winfo_screenwidth()
@@ -43,127 +21,136 @@ main.geometry('550x700')
 
 center(main)
 
+BIRD_Y = 200
+PIPE_X = 550
+PIPE_HOLE = 0
+NOW_PAUSE = False
+
+BEST_SCORE = 0
+
+if os.path.isfile("data.dat"):
+	scoreFile = open('data.dat')
+	BEST_SCORE = int(scoreFile.read())
+	scoreFile.close()
+else:
+	scoreFile = open('data.dat', 'w')
+	scoreFile.write(str(BEST_SCORE))
+	scoreFile.close()
+
 w = Canvas(main, width = 550, height = 700, background = "#4EC0CA", bd=0, highlightthickness=0)
 w.pack()
 
 birdImg = PhotoImage(file="images/bird.gif")
-bird = w.create_image(100, current_x, image=birdImg)
+bird = w.create_image(100, BIRD_Y, image=birdImg)
 
-b1 = w.create_rectangle(truba_x, 0, truba_x + 100, diraP, fill="#74BF2E", outline="#74BF2E")
-b2 = w.create_rectangle(truba_x, diraP + 200, truba_x + 100, 700, fill="#74BF2E", outline="#74BF2E")
+up_count = 0
+endRectangle = endBest = endScore = None
+
+pipeUp = w.create_rectangle(PIPE_X, 0, PIPE_X + 100, PIPE_HOLE, fill="#74BF2E", outline="#74BF2E")
+pipeDown = w.create_rectangle(PIPE_X, PIPE_HOLE + 200, PIPE_X + 100, 700, fill="#74BF2E", outline="#74BF2E")
 score_w = w.create_text(15, 45, text="0", font='Impact 60', fill='#ffffff', anchor=W)
 
 
-def updateDira():
-	global diraP
-	global score
-	global FRAME_TIME
+def generatePipeHole():
+	global PIPE_HOLE
+	global SCORE
+	global FRAMERATE
+	SCORE += 1
+	w.itemconfig(score_w, text=str(SCORE))
+	PIPE_HOLE = random.randint(50, 500)
+	if SCORE + 1 % 7 == 0 and SCORE != 0: 
+		FRAMERATE-=1
+	#print("Score: " + str(SCORE))
+
+generatePipeHole()
+
+def birdUp(event = None):
+	global BIRD_Y
+	global up_count
+	global NOW_PAUSE
 	
-	diraP = random.randint(50, 500)
-	score += 1
-	w.itemconfig(score_w, text=str(score))
-	if (score + 1) % 5 == 0: FRAME_TIME -= 0.001
+	if NOW_PAUSE == False: 
+		BIRD_Y -= 20
+		if BIRD_Y <= 0: BIRD_Y = 0
+		w.coords(bird, 100, BIRD_Y)
+		if up_count < 5:
+			up_count += 1
+			main.after(FRAMERATE, birdUp)
+			
+		else: up_count = 0
+	else:
+		restartGame()
 
-updateDira()
+def birdDown():
+	global BIRD_Y
+	global NOW_PAUSE
 
-def down():
-	global current_x
-	global truba_x
-	global state
+	BIRD_Y += 8
+	if BIRD_Y >= 700: BIRD_Y = 700
+	w.coords(bird, 100, BIRD_Y)
+	if NOW_PAUSE == False: main.after(FRAMERATE,birdDown)
+
+def pipesMotion():
+	global PIPE_X
+	global PIPE_HOLE
+	global NOW_PAUSE
+
+	PIPE_X -= 5
+	w.coords(pipeUp, PIPE_X, 0, PIPE_X + 100, PIPE_HOLE)
+	w.coords(pipeDown, PIPE_X, PIPE_HOLE + 200, PIPE_X + 100, 700)
+	
+	if PIPE_X < -100: 
+		PIPE_X = 550
+		generatePipeHole()
+	
+	if NOW_PAUSE == False: main.after(FRAMERATE, pipesMotion)
+
+def engGameScreen():
+	global endRectangle
+	global endScore
+	global endBest
+	endRectangle = w.create_rectangle(0, 0, 550, 700, fill='#4EC0CA')
+	endScore = w.create_text(15, 200, text="Your score: " + str(SCORE), font='Impact 50', fill='#ffffff', anchor=W)
+	endBest = w.create_text(15, 280, text="Best score: " + str(BEST_SCORE), font='Impact 50', fill='#ffffff', anchor=W)
+
+def detectCollision():
+	global NOW_PAUSE
 	global BEST_SCORE
-	global score
-	global finT1
-	global finT2
-	global finR
-	global IS_PAUSE
 
-	while True:
-		#print("dude" + str(random.randint(75, 999)))
-		if state == "close": break
-		if IS_PAUSE != True:
-			if (truba_x < 150 and truba_x + 100 >= 55) and (current_x < diraP + 45 or current_x > diraP + 175):
-				'''messagebox.showerror("You loose(", "You loose( Your score " + str(score))
-				'''
-				IS_PAUSE = True
-				if score > BEST_SCORE:
-					BEST_SCORE = score
-					scoreFile = open('data.dat', 'w')
-					scoreFile.write(str(BEST_SCORE))
-					scoreFile.close()
+	if (PIPE_X < 150 and PIPE_X + 100 >= 55) and (BIRD_Y < PIPE_HOLE + 45 or BIRD_Y > PIPE_HOLE + 175):
+		#print("Collision")
+		NOW_PAUSE = True
+		if SCORE > BEST_SCORE:
+			BEST_SCORE = SCORE
+			scoreFile = open('data.dat', 'w')
+			scoreFile.write(str(BEST_SCORE))
+			scoreFile.close()
+		#print("Pause")
+		engGameScreen()
+	if NOW_PAUSE == False: main.after(FRAMERATE, detectCollision)
 
-					
-				finR = w.create_rectangle(0, 0, 550, 700, fill='#4EC0CA')
-				finT1 = w.create_text(15, 200, text="Your score: " + str(score), font='Impact 50', fill='#ffffff', anchor=W)
-				finT2 = w.create_text(15, 280, text="Best score: " + str(BEST_SCORE), font='Impact 50', fill='#ffffff', anchor=W)
+def restartGame():
+	global PIPE_X
+	global BIRD_Y
+	global SCORE
+	global NOW_PAUSE
+	global FRAMERATE
 
-			else:
-				if(state != "up"):
-					if current_x < 0: current_x = 0
-					if current_x > 700: current_x = 680
-					w.coords(bird, 100, current_x)
-					current_x = int(current_x + 9)
-				truba_x -= 4
-				if(truba_x <= -100): 
-					truba_x = 550
-					updateDira()
-				w.coords(b1, truba_x, 0, truba_x + 100, diraP)
-				w.coords(b2, truba_x, diraP + 200, truba_x + 100, 700)
-		time.sleep(FRAME_TIME)
-
-def up():
-	global state
-	global current_x
-
-	state = "up"
-	for i in range(5):
-		
-		if current_x < 0: break
-		current_x = int(current_x - 17)
-		w.coords(bird, 100, current_x)
-		time.sleep(FRAME_TIME)
-	state = "norm"	
-
-def upKey(event = None):
-	global finT1
-	global finT2
-	global finR
-	global state
-	global score
-	global truba_x
-	global current_x
-	global IS_PAUSE
-	global FRAME_TIME
-
-	if IS_PAUSE == True:
-		score = -1
-		truba_x = 300
-		current_x = 200
-		FRAME_TIME = 0.022
-		updateDira()
-		w.delete(finT1)
-		w.delete(finT2)
-		w.delete(finR)
-		IS_PAUSE = False
-
-	elif state != "up":
-		t1 = Thread(target = up)
-		t1.start()
-
-def close(event = None):
-	global main
-	global state
-	state = "close"
+	BIRD_Y = 200
+	PIPE_X = 550
+	SCORE = -1
+	FRAMERATE = 20
+	NOW_PAUSE = False
+	w.delete(endScore)
+	w.delete(endRectangle)
+	w.delete(endBest)
+	generatePipeHole()
+	main.after(FRAMERATE, birdDown)
+	main.after(FRAMERATE, pipesMotion)
+	main.after(FRAMERATE, detectCollision)
 	
-	main.destroy()
-	main.quit()
-	
-	
-
-t = Thread(target = down)
-t.start()
-
-main.bind("<space>", upKey)
-main.bind("<Escape>", close)
-
-main.protocol("WM_DELETE_WINDOW", close)
+main.after(FRAMERATE, birdDown)
+main.after(FRAMERATE, pipesMotion)
+main.after(FRAMERATE, detectCollision)
+main.bind("<space>", birdUp)
 main.mainloop()
