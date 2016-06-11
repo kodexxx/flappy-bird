734098a216fc8ab01b9611ee2a9ace044f438a41
potterua <nazarrrka@gmail.com>
2016-06-11 17:56:03 +0300

    Update game.py

--- game.py
+++ game.py
@@ -6,13 +6,13 @@ FRAMERATE = 20
 SCORE = -1
 
 def center(toplevel):
-    toplevel.update_idletasks()
-    w = toplevel.winfo_screenwidth()
-    h = toplevel.winfo_screenheight()
-    size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
-    x = w/2 - size[0]/2
-    y = h/2 - size[1]/2 - 35
-    toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
+	toplevel.update_idletasks()
+	w = toplevel.winfo_screenwidth()
+	h = toplevel.winfo_screenheight()
+	size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
+	x = w/2 - size[0]/2
+	y = h/2 - size[1]/2 - 35
+	toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
 	
 main = Tk()
 main.resizable(width = False, height = False)
